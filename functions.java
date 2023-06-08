b99cb113f14b72e5ae0340b34ca23c183551494e
TommyMo1512 <haminhnguyentran@gmail.com>
2023-06-08 22:33:37 +0700

    Add subtract

--- functions.java
+++ functions.java
@@ -3,10 +3,11 @@ public class functions {
     public static void main(String[] args) {  
       System.out.println("Enter the Two numbers for addtion: ");  
       Scanner readInput = new Scanner(System.in);  
-      int a = readInput.nextInt();  
-        int b = readInput.nextInt();  
+      long a = readInput.nextInt();  
+        long b = readInput.nextInt();  
         readInput.close();  
         // It will return the sum of a and b.  
-        System.out.println("The sum of a and b is = " + Integer.sum(a, b));  
+        System.out.println("The sum of a and b is = " + Math.subtractExact(a, b));  
     }  
 }  
+ 
